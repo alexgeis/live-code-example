10c977dc31b8c8a7038d9145e8413086cb415873
Alex Geis <siegxela@gmail.com>
2022-05-06 16:44:17 -0600

    import/replace header in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div classNameName="App">
+      <Header />
       <nav>
         <a href="#">Nav Link 1</a>
         <a href="#">Nav Link 2</a>
