b845c5633de0db44e6e622b1a205fde61e0923ee
Alex Geis <siegxela@gmail.com>
2022-05-06 15:50:12 -0600

    update aside template text and comment out card example

--- client/src/App.js
+++ client/src/App.js
@@ -21,38 +21,19 @@ function App() {
 
       <main>
         <aside>
-          <h2>The product cards would look better if...</h2>
-          <li>the corners were rounded out</li>
-          <li>there was a shadow</li>
-          <li>they were rotated slightly</li>
+          <h2>Live Code Editor Section Title</h2>
+          <li>note 1 on this attempt</li>
+          <li>note 2 on this attempt</li>
+          <li>note 3 on this attempt</li>
         </aside>
+
         <div className="products">
-          <section className="card">
-            <header>Camera</header>
-            <img src="./assets/images/camera.jpg" alt="black camera" />
+          {/* CARD TEMPLATE */}
+          {/* <section className="card">
+            <header>Kitten for sale</header>
+            <img src="http://placekitten.com/200/300" alt="cute kitten" />
             <p>Price: $300</p>
-          </section>
-          <section className="card">
-            <header>Tablet</header>
-            <img src="./assets/images/tablet.jpg" alt="tablet held in hands" />
-            <p>Price: $150</p>
-          </section>
-          <section className="card">
-            <header>Toilet Paper</header>
-            <img
-              src="./assets/images/toilet-paper.jpg"
-              alt="rolls of toilet paper in a basket"
-            />
-            <p>Price: $30</p>
-          </section>
-          <section className="card">
-            <header>Wooden Spoons</header>
-            <img
-              src="./assets/images/wooden-spoons.jpg"
-              alt="4 wooden spoons with different seasonings"
-            />
-            <p>Price: $10</p>
-          </section>
+          </section> */}
         </div>
       </main>
 
